eb914f654218b7c85b835a4006f606c29aff231c
Tems17 <funmibipopoola@gmail.com>
2025-09-17 18:32:29 -0400

    Add files via upload

---
+++ Assignment1/config.php
@@ -0,0 +1,7 @@
+<?php
+//Dog API Key
+define("DOG_API_KEY", "live_kXzbOiwGBbKSzNLeTrrkEhK1RKryBcVAabFz9bXlUSsn9VAcL2r0hBYfygVOuDSc");
+
+//Base URL 
+define("DOG_BASE_URL", "https://api.thedogapi.com/v1");
+?>
